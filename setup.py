de2b1692d175b3febc9e87de08b732b42028a4ea
indaco biazzo <ocadni@users.noreply.github.com>
2022-02-01 13:44:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="ANNforEpi",
+    name="ANNforE",
     version="0.1",
-    author="sibyl-team",
+    author="Indaco Biazzo, Fabio Mazza",
     packages=find_packages(),
     description="Epidemic inference with autoregressive neural networks",
     install_requires=[
